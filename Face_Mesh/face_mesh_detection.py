b65ae80efb026e945b17e7be673eafeaf3fc5c61
jjf2009 <jaredfurtadowork@gmail.com>
2025-01-10 15:31:24 +0530

    file added

--- Face_Mesh/face_mesh_detection.py
+++ Face_Mesh/face_mesh_detection.py
@@ -1,16 +1,12 @@
 import cv2 as cv
 import mediapipe as mp 
 import time 
-
 mp_face_mesh=mp.solutions.face_mesh
 mp_drawing =mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
-
 pTime=0
 cTime=0
-
 cap= cv.VideoCapture(0)
-
 with mp_face_mesh.FaceMesh(  static_image_mode=False,max_num_faces=1,refine_landmarks=True, min_detection_confidence=0.5) as face_mesh:
     while cap.isOpened():
         success,image = cap.read()
